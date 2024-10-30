ea3258af576f688a2fb63df9337bb09b4b2190e3
Tomas Messineo <tomasnahuelmessineo@gmail.com>
2024-10-30 14:06:53 -0300

    Cambios en la regex de congresos

--- Expression/Analyzers/TitleAnalyzer/TitleExpressions/CongressExpression.php
+++ Expression/Analyzers/TitleAnalyzer/TitleExpressions/CongressExpression.php
@@ -4,7 +4,7 @@ include_once('GenericExpression.php');
 
     class CongressExpression extends GenericExpression{
 
-        private static $congressPattern = ['/\)\.\s(?P<title>.+?)\s\[(?P<comment>[^,\]]+)\]\.\s(?P<evento>[A-Z][^\.]*)\.\s(?P<publishername>[A-Z][^\.!,]*\.)?\s?(?P<publisherloc>[A-Z][^\.]*)?/' => 'congress'];
+        private static $congressPattern = ['/\)\.\s(?P<title>.+?)\s\[(?P<comment>[^,\]]+)\]\.\s(?P<event>[A-Z][^\.]*)\.\s(?P<publishername>[A-Z][^\.!,]*\.)?\s?(?P<publisherloc>[A-Z][^\.]*)?/' => 'congress'];
 
         public static function getPattern(){
             return self::$congressPattern;
