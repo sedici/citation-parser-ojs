4618196d3f2005ae5153ed503b0c7dc2e70d2c9b
Tomas Messineo <tomasnahuelmessineo@gmail.com>
2024-11-01 09:29:46 -0300

    Add (Author): Added institution.

--- Expression/Analyzers/AuthorAnalyzer/AuthorPatterns.php
+++ Expression/Analyzers/AuthorAnalyzer/AuthorPatterns.php
@@ -2,22 +2,26 @@
 
     class AuthorPatterns {
 
-        public static function getPatterns () {
+        public static function getAuthorPattern(){
 
-            //Author Pattern.
-            $apellido = "(?P<apellido>[A-Za-zÀ-ÿñÑ]+(?:\s[A-Za-zÀ-ÿñÑ]+)*)";
-            $nombre = "(?P<nombres>(\p{Lu}\.\s?)+)";
-    
-            $role = "(?P<role>(\((Ed.|Coord.|Comp.)\)))?";  //Los roles deberian ser individuales 
-            $roles = "(?P<roles>(\((Eds.|Coords.|Comps.)\)))?";   
-    
-            $author = "(?P<author>".$apellido.", ".$nombre." ".$role.")"; //Corregir los espacios
-            $authors = '/'.$author.$roles.'((?:,\s|y\s))?/u';
+           //Author Pattern.
+           $apellido = "(?P<apellido>[A-Za-zÀ-ÿñÑ]+(?:\s[A-Za-zÀ-ÿñÑ]+)*)";
+           $nombre = "(?P<nombres>(\p{Lu}\.\s?)+)";
+   
+           $role = "(?P<role>(\((Ed.|Coord.|Comp.)\)))?";  //Los roles deberian ser individuales 
+           $roles = "(?P<roles>(\((Eds.|Coords.|Comps.)\)))?";   
+   
+           $author = "(?P<author>".$apellido.", ".$nombre.$role.")"; //Corregir los espacios
+           $authors = '/'.$author.$roles.'((?:,\s|y\s|&\s))?/';
 
-            //Institution Pattern.
+           return $authors;
+        }
+
+        public static function getInstitutionPattern(){
             
+           //Institution Pattern.
 
-            return $authors;
+           return null;
 
         }
 
